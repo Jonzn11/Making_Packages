34a418368020da0528d40effe5cb6ed3923e723e
Andrew <108536970+Jonzn11@users.noreply.github.com>
2022-10-16 21:03:06 +0600

    Create geometry.py

---
+++ geometry.py
@@ -0,0 +1,12 @@
+def area_of_rectangle(number)
+    """
+    This function returns the area of a area_of_rectangle
+    """
+    return number (a+b)
+
+
+def area_of_circle(number)
+    """
+    This function returns the area of a area of a circle.
+    """
+    return number * 3.14159
